0d752519abc35e7e2857038348e39376a835b7c0
CryTime <zoulou.snmarn@laposte.net>
2019-10-19 03:10:41 +0200

    composant rencontre avec le swipe

---
+++ front/src/app/rencontres/rencontres.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rencontres',
+  templateUrl: './rencontres.component.html',
+  styleUrls: ['./rencontres.component.css']
+})
+export class RencontresComponent implements OnInit {
+  config: any = {
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    spaceBetween: 60
+  };
+  	
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
