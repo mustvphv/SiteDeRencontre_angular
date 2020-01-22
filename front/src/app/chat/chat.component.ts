365b5dda5ab294d4e4ec6a8d02b582148a053c55
mustvphv <mustaphailyes.dahmani@gmail.com>
2020-01-22 11:03:28 +0100

    refonte messagerie front

--- front/src/app/chat/chat.component.ts
+++ front/src/app/chat/chat.component.ts
@@ -65,6 +65,7 @@ export class ChatComponent implements OnInit {
 
 
     if (this.chargementHistoriqueMessages === 0) {
+      
         this.httpClient.get(`http://localhost:3000/chat/historiqueMessages/${this.pseudo}/${this.pseudo2}`, { headers: new HttpHeaders({
           'Authorization': `Bearer ${valeurAuthToken}`,
           'Content-Type': 'application/json',
@@ -72,14 +73,88 @@ export class ChatComponent implements OnInit {
         // .pipe(takeLast(1))
         .subscribe((data: any) => {
 
-          if (data[0] != null) {
+          let tempTexte = [];
+          let tempDate = [];
+          let tempDate2 = [];
+          let tempTexte2 = [];
+          if (data[0] != null && data[0] !== undefined && data[0] !== data[0].length > 0) {
 
-          let tempTexte = [data[0].message1Texte, data[0].message2Texte, data[0].message3Texte, data[0].message4Texte,
-            data[0].message5Texte, data[0].message6Texte, data[0].message7Texte, data[0].message8Texte, data[0].message9Texte, data[0].message10Texte];
 
-          let tempDate = [data[0].message1Date, data[0].message2Date, data[0].message3TexteDate, data[0].message4Date,
+            for(let i = 0; i < data[0].length; i++) {
+              tempTexte.push(data[0][i].messageTexte);
+              tempDate.push(data[0][i].messageDate);
+            }
+
+
+            for (let i = 0; i < tempTexte.length; i++) {
+              if (tempDate[i] !== 'null' && tempDate[i] != null) {
+                  let messageBDD1 = {texte: '', source: '', date: ''};
+                  messageBDD1.texte = JSON.parse(JSON.stringify(tempTexte[i]));
+                  messageBDD1.source = this.pseudo;
+                  messageBDD1.date = JSON.parse(JSON.stringify(tempDate[i]));
+                  this.messagesBDD.push(messageBDD1);
+              }
+            }
+          }
+
+          if (data[1] != null && data[1] !== undefined && data[1] !== data[1].length > 0) {
+
+
+            for(let i = 0; i < data[1].length; i++) {
+              tempTexte2.push(data[1][i].messageTexte);
+              tempDate2.push(data[1][i].messageDate);
+            }
+
+
+            for (let i = 0; i < tempTexte2.length; i++) {
+              if (tempDate2[i] !== 'null' && tempDate2[i] != null) {
+                  let messageBDD2 = {texte: '', source: '', date: ''};
+                  messageBDD2.texte = JSON.parse(JSON.stringify(tempTexte2[i]));
+                  messageBDD2.source = this.pseudo2;
+                  messageBDD2.date = JSON.parse(JSON.stringify(tempDate2[i]));
+                  this.messagesBDD.push(messageBDD2);
+              }
+            }
+          }
+
+          if (this.messagesBDD.length > 1) {
+              let changements = 0;
+              while (changements > -1) {
+              changements = 0;
+              for (let i = 0; i < this.messagesBDD.length - 1; i++) {
+                let date1 = new Date(this.messagesBDD[i].date.split('|')[0]);
+                let date2 = new Date(this.messagesBDD[i + 1].date.split('|')[0]);
+                //console.log('date1getTime' + date1.getTime());
+                // console.log('date2getTime' + date2.getTime());
+                if (date1.getTime() > date2.getTime()) {
+                    let tmp = this.messagesBDD[i];
+                    this.messagesBDD[i] = this.messagesBDD[i + 1];
+                    this.messagesBDD[i + 1] = tmp;
+                    changements ++;
+                }
+              }
+              if (changements === 0) {
+                  changements = -1;
+              }
+              }
+            }
+  
+          for (let i = 0; i < this.messagesBDD.length; i++) {
+              this.messagesBDD[i].date = this.messagesBDD[i].date.split('|')[1];
+  
+              this.messages.push(this.messagesBDD[i]);
+            }
+
+          /*console.log('data[0].length' + data[0].length);
+          for(let i = 0; i < data[0].length; i++) {
+                let tempTexte = [data[0].messageTexte, data[0].message2Texte, data[0].message3Texte, data[0].message4Texte,
+                data[0].message5Texte, data[0].message6Texte, data[0].message7Texte,
+                data[0].message8Texte, data[0].message9Texte, data[0].message10Texte];
+
+                let tempDate = [data[0].message1Date, data[0].message2Date, data[0].message3TexteDate, data[0].message4Date,
             data[0].message5Date, data[0].message6Date, data[0].message7Date, data[0].message8Date,
             data[0].message9Date, data[0].message10Date];
+          }
 
           for (let i = 0; i < 10; i++) {
             if (tempDate[i] !== 'null' && tempDate[i] != null) {
@@ -142,7 +217,7 @@ export class ChatComponent implements OnInit {
             this.messagesBDD[i].date = this.messagesBDD[i].date.split('|')[1];
 
             this.messages.push(this.messagesBDD[i]);
-          }
+          }*/
 
 
 
@@ -183,26 +258,8 @@ export class ChatComponent implements OnInit {
       .post('http://localhost:3000/chat/enregistrementMessages', {
         pseudoSource: this.pseudo,
         pseudoDest: this.pseudo2,
-        message1Texte: this.message.texte,
-        message1Date: this.message.date,
-        message2Texte: 'null',
-        message2Date: 'null',
-        message3Texte: 'null',
-        message3Date: 'null',
-        message4Texte: 'null',
-        message4Date: 'null',
-        message5Texte: 'null',
-        message5Date: 'null',
-        message6Texte: 'null',
-        message6Date: 'null',
-        message7Texte: 'null',
-        message7Date: 'null',
-        message8Texte: 'null',
-        message8Date: 'null',
-        message9Texte: 'null',
-        message9Date: 'null',
-        message10Texte: 'null',
-        message10Date: 'null'
+        messageTexte: this.message.texte,
+        messageDate: this.message.date
       })
       .pipe(
         timeout(2000),
